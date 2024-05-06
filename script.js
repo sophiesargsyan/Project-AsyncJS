772748af10c6d2259078750919c158f21da3ba14
Sophie Sargsyan <144236558+sophiesargsyan@users.noreply.github.com>
2024-05-07 00:39:59 +0400

    sendData function is created instead of userLogin and login functions

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 'use strict';
+const email = prompt("Enter your email:");
+const password = prompt("Enter your password:");
 
-function login(email, password) {
+function sendData(email, password){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            const randomNumber = Math.floor(Math.random() * 2);
-            if (randomNumber > 0) {
-                resolve("Welcome to your account");
-            } else {
-                reject("Incorrect email or password");
-            }
+            Math.random() * 3 > 1 
+            ? resolve("Welcome to your account") 
+            : reject("Incorrect email or password");
         }, 2000);
     });
 }
-async function userLogin() {
-    const email = prompt("Enter your email:");
-    const password = prompt("Enter your password:");
 
-    try {
-        const result = await login(email, password);
-        alert(result);
-    } catch (error) {
+sendData(email, password)
+    .then(response => {
+        alert(response);
+    })
+    .catch(error => {
         alert(error);
-    }
-}
-
-userLogin()
+    });
